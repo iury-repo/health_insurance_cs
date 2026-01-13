30c481c9a10e10414f696690dcec244c076d85d7
Iury <iurymartinssilva7@gmail.com>
2026-01-13 11:36:48 -0300

    refactor(bugfix): Remove useless data split and Fix age MinMaxScalerbeing used for the vintage feature

--- insurance_classifier/dataset.py
+++ insurance_classifier/dataset.py
@@ -37,50 +37,47 @@ from insurance_classifier.config import PROCESSED_DATA_DIR, RAW_DATA_DIR
 
 def DataPreprocessing(df):
 
-    X_df = df.drop(['id','response'], axis=1)
-    y_df = df['response'].copy()
-
     # Rename vehicle_age categorie
-    X_df['vehicle_age'] = X_df['vehicle_age'].map({'> 2 Years': 'over_2_years',
+    df['vehicle_age'] = df['vehicle_age'].map({'> 2 Years': 'over_2_years',
                                                         '1-2 Year': '1_to_2_years',
                                                         '< 1 Year': 'under_1_year'
                                                         })
 
     # Change string entries to snakecase
-    for col in X_df.select_dtypes(exclude=['int64','float64', 'datetime64[ns]']).columns:
-        X_df[col] = X_df[col].str.lower()
+    for col in df.select_dtypes(exclude=['int64','float64', 'datetime64[ns]']).columns:
+        df[col] = df[col].str.lower()
 
     # Encoding -----
 
     # One-Hot encoding
-    X_df = pd.get_dummies(X_df, columns= ['gender'], prefix= 'gender', dtype=int)
+    df = pd.get_dummies(df, columns= ['gender'], prefix= 'gender', dtype=int)
 
     # Target encoding (James-Stein) 
     tg_enc_region_code = ce.JamesSteinEncoder(cols=['region_code'])
-    X_df['region_code'] = tg_enc_region_code.fit_transform(X= X_df[['region_code']], y= y_df)
+    df['region_code'] = tg_enc_region_code.fit_transform(X= df[['region_code']])
 
     # Label encoding
-    X_df['vehicle_damage'] = X_df['vehicle_damage'].map({'yes':1,'no':0})
+    df['vehicle_damage'] = df['vehicle_damage'].map({'yes':1,'no':0})
 
     # One-Hot encoding
-    X_df = pd.get_dummies(X_df, columns= ['vehicle_age'], prefix= 'vehicle_age', dtype=int)
+    df = pd.get_dummies(df, columns= ['vehicle_age'], prefix= 'vehicle_age', dtype=int)
 
     # Frequency encoding
-    fe_policy_sales_channel = X_df['policy_sales_channel'].value_counts(normalize=True)
-    X_df['policy_sales_channel'] = X_df['policy_sales_channel'].map(fe_policy_sales_channel)
+    fe_policy_sales_channel = df['policy_sales_channel'].value_counts(normalize=True)
+    df['policy_sales_channel'] = df['policy_sales_channel'].map(fe_policy_sales_channel)
 
     # Rescaling -----
 
     mms_age = MinMaxScaler()
     mms_vintage = MinMaxScaler()
 
-    X_df['age'] = mms_age.fit_transform(X_df[['age']].values)
-    X_df['vintage'] = mms_age.fit_transform(X_df[['vintage']].values)
+    df['age'] = mms_age.fit_transform(df[['age']].values)
+    df['vintage'] = mms_vintage.fit_transform(df[['vintage']].values)
 
     # Standardization -----
 
     # annual_premium
     ss = StandardScaler()
-    X_df['annual_premium'] = ss.fit_transform(X_df[['annual_premium']].values)
+    df['annual_premium'] = ss.fit_transform(df[['annual_premium']].values)
 
     return df
